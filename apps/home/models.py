d23258973134873239b1f58e578c6c65a3988051
TSIDIANY Raharison Muriel <106076190+ITDevSuccess@users.noreply.github.com>
2024-04-27 17:58:25 +0300

    Ajout de Settings de grids

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Hebdo(models.Model):
+    department = models.CharField(max_length=150)
+    family = models.CharField(max_length=150)
+    article = models.CharField(max_length=150)
+    designation = models.TextField()
+    type = models.CharField(max_length=50, choices=(
+        ('Import', 'Import'),
+        ('Local', 'Local'),
+    ))
+    qte_pta = models.DecimalField(max_digits=20, decimal_places=2)
+    pmp_pta = models.DecimalField(max_digits=20, decimal_places=2)
+    pdv_pta = models.DecimalField(max_digits=20, decimal_places=2)
+    qte_realisation = models.DecimalField(max_digits=20, decimal_places=2)
+    pmp_realisation = models.DecimalField(max_digits=20, decimal_places=2)
+    pdv_realisation = models.DecimalField(max_digits=20, decimal_places=2)
+    ecart_qte = models.DecimalField(max_digits=20, decimal_places=2)
+    ecart_ca_ht = models.DecimalField(max_digits=20, decimal_places=2)
+    stock_prevision = models.DecimalField(max_digits=20, decimal_places=2)
+
